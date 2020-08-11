1a5bef775e72a20edad577acd6db3fd5984963c0
KamilSzostak <kamil.szostak.1997@gmail.com>
2020-08-11 14:17:43 +0200

    Both quizes working

--- quiz/models.py
+++ quiz/models.py
@@ -8,20 +8,26 @@ class Question(models.Model):
 	Option2=models.CharField(max_length=150)
 	Option3=models.CharField(max_length=150)
 	Option4=models.CharField(max_length=150)
-	Correct=models.CharField(max_length=150)
+
 
 
 class PoliticalViews(Question):
+	PizzaAns=models.CharField(max_length=150)
+	BurgerAns=models.CharField(max_length=150)
+	KebabAns=models.CharField(max_length=150)
+	HotDogAns=models.CharField(max_length=150)
 	
 	def __str__(self):
 		return self.Content
 
 class IQTest(Question):
+	Correct=models.CharField(max_length=150)
 	
 	def __str__(self):
 		return self.Content
 
 class FastFood(Question):
-	
+	Correct=models.CharField(max_length=150)
+
 	def __str__(self):
 		return self.Content
